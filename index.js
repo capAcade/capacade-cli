c391cb5767960913c770ac4e478aebe1b85b482d
sven van straalen <sven.van.straalen@capgemini.com>
2021-07-23 15:10:11 +0200

    manual mode it iss

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
+const inquirer = require('inquirer');
+const fs = require('fs');
 console.log('hi this is a test');
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function (input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  }
+];
+
+
+inquirer.prompt(QUESTIONS)
+  .then(answers => {
+    console.log(answers);
+});
